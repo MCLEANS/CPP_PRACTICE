aab180b74c5ed43fa4659d2e504d7d55837e3a78
MCLEANS <jackagwaya@gmail.com>
2020-08-23 22:54:35 +0300

    Imlemented returning objects from functions using new operator

--- NEW_OPERATOR/RETURNING_OBJECTS_FROM_FUNCTIONS/main.cpp
+++ NEW_OPERATOR/RETURNING_OBJECTS_FROM_FUNCTIONS/main.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include "student.h"
 
-int main(int argc, char **argv){
-
+Student* create_absent_student(){
     Student *absent_student = new Student;
 
     absent_student->set_name("JACK MCLEANS");
     absent_student->set_course("ENGINEERING");
     absent_student->set_age(25);
 
+    return absent_student;
+
+}
+
+int main(int argc, char **argv){
+
+    Student *absent_student = create_absent_student();
     std::cout<<absent_student->get_course()<<std::endl;
 
     delete absent_student;
-
-    
 
     return 0;
 }
