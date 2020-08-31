5843339122a695dfceaf56b26d5e4fd131d2898d
MCLEANS <jackagwaya@gmail.com>
2020-08-31 16:40:33 +0300

    primer implementation

--- NEW_OPERATOR/PRIMER/main.cpp
+++ NEW_OPERATOR/PRIMER/main.cpp
@@ -3,7 +3,13 @@
 
 int main(int argc, char **argv){
 
-    std::cout<<"Hello World"<<std::endl;
+   Student *absentees = new Student[10];
+
+   absentees[0].set_registration("EEEQ/0");
+   absentees[1].set_registration("EEEQ/1");
+
+   std::cout<<"Absentee 0 registration : "<<absentees[0].get_registration()<<std::endl;
+   std::cout<<"Absentee 1 registration : "<<absentees[1].get_registration()<<std::endl;
 
     return 0;  
 }
