cea7b4fb3bfa4dd7ada2bd022b200bb0a8314430
MCLEANS <jackagwaya@gmail.com>
2020-09-02 15:48:37 +0300

    added alternatives

--- FUNCTION_POINTERS/INTRO/main.cpp
+++ FUNCTION_POINTERS/INTRO/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+int add(int a,int b){
+    return (a+b);
+}
+
 int main(int agrc, char* argv[]){
 
-std::cout<<"HELLO WORLD"<<std::endl;
+    //create a function pointer
+    //return type(*variable)(parameter types)
+    int(*add_pointer)(int,int);
+    add_pointer =  &add; //similar to add_pointer = add;
+    int result = (*add_pointer)(3,4); //similar to add_pointer(3,4);
+
+    std::cout<<"RESULT: "<<result<<std::endl;
+
 
     return 0;
 }
