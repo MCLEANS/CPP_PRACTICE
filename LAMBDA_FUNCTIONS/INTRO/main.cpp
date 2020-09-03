3cb91bebfb0d664b31d604ec12240da02a271421
MCLEANS <jackagwaya@gmail.com>
2020-09-03 09:39:25 +0300

    implemented passing capture list by value and by reference

--- LAMBDA_FUNCTIONS/INTRO/main.cpp
+++ LAMBDA_FUNCTIONS/INTRO/main.cpp
@@ -5,5 +5,19 @@ int main(int argc, char *argv[]){
 
     std::cout<<"HELLO WORLD"<<std::endl;
 
+    int birth_year = 1996;
+    int current_year = 2020;
+
+    auto calculate_age = [=,&birth_year](){
+        birth_year = 1990;
+        int age = current_year - birth_year;
+        std::cout<<"You are "<<age<<" years old"<<std::endl;
+    };
+
+    calculate_age();
+
+    std::cout<<"birth year: "<<birth_year<<std::endl;
+    std::cout<<"current year: "<<current_year<<std::endl;
+
     return 0;
 }
