c58ac1544b20804e91af9cf9886d3a2f1fabe5e4
MCLEANS <jackagwaya@gmail.com>
2020-09-03 09:58:30 +0300

    implemented lambda functions return type

--- LAMBDA_FUNCTIONS/LAMBDA_RETURN_TYPES/main.cpp
+++ LAMBDA_FUNCTIONS/LAMBDA_RETURN_TYPES/main.cpp
@@ -4,5 +4,17 @@ int main(int argc, char **argv){
 
     std::cout<<"HELLO WORLD"<<std::endl;
 
+    int birth_year = 1996;
+    int current_year = 2020;
+
+    auto calculate_age = [current_year,birth_year]()->int{
+        int age = current_year-birth_year;
+        return age;
+    };
+
+    int age = calculate_age();
+
+    std::cout<<"You are "<<age<<" years old"<<std::endl;
+
     return 0;
 }
