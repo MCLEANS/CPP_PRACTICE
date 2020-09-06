418f490068fe8f990c0d38f024b607f6ec89419f
MCLEANS <jackagwaya@gmail.com>
2020-09-06 16:43:42 +0300

    created get request loop

--- HTTP_REQUESTS/INTRO/main.cpp
+++ HTTP_REQUESTS/INTRO/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include "cpr/cpr.h"
+#include <typeinfo>
 
 
 int main(int argc, char *argv[]){
 
 	std::cout<<"HELLO WORLD"<<std::endl;
 
-	 cpr::Response r = cpr::Get(cpr::Url{"https://tukdata.herokuapp.com/data/real-time"});
-	 std::cout<<r.text<<std::endl;
+	std::string name;
+
+	while (1)
+	{
+		cpr::Response r = cpr::Get(cpr::Url{"https://tukdata.herokuapp.com/data/real-time"});
+	 	std::cout<<r.text<<std::endl;
+	}
+	
+
 
 	return 0;
 }
