3ec40095640c61db5c88de533a67a22ad09b6795
MCLEANS <jackagwaya@gmail.com>
2020-09-06 16:22:41 +0300

    simple get request implementation

--- HTTP_REQUESTS/INTRO/main.cpp
+++ HTTP_REQUESTS/INTRO/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "cpr/cpr.h"
 
 
 int main(int argc, char *argv[]){
 
 	std::cout<<"HELLO WORLD"<<std::endl;
+
+	 cpr::Response r = cpr::Get(cpr::Url{"https://tukdata.herokuapp.com/data/real-time"});
+	 std::cout<<r.text<<std::endl;
 
 	return 0;
 }
