d546741c9f75ec9b3ed8dc509a935d1c3d31a8f5
MCLEANS <jackagwaya@gmail.com>
2020-09-14 13:08:00 +0300

    implemented primer1

--- LAMBDA_FUNCTIONS/PRIMER_1/main.cpp
+++ LAMBDA_FUNCTIONS/PRIMER_1/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 
+/**********************************
+ * LAMBDA FUNCTIONS IMPLEMENTATION
+ * ********************************/
+
+int find_max(int *array, size_t size){
+	int max = *array;
+	for(size_t i = 0; i < size; i++,array++){
+		if (*array > max) max = *array;
+	}
+	return max;
+}
+
+int find_min(int *array, size_t size){
+	int min = *array;
+	for(size_t i = 0; i < size; i++,array++){
+		if(*array < min) min = *array; 
+	}
+	return min;
+}
 
 int main(int argc, char **argv){
 
-	std::cout<<"HELLO WORLD"<<std::endl;
+	int marks[] = {12,32,45,76,34,87,65,34,66,89,45,877,56,544,33,34};
+
+	auto find_n = [&marks](int(*find)(int* , size_t))->int{
+		int n = find(marks,sizeof(marks)/sizeof(int));
+		return n;
+	};
+
+	std::cout<<"Maximum: "<<find_n(find_max)<<std::endl;
+	std::cout<<"Minimum: "<<find_n(find_min)<<std::endl;
 
 	return 0;
 }
