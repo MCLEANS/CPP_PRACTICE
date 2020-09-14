6c01c6d99a34580b65fb20c5fd3e6088eb7c1392
MCLEANS <jackagwaya@gmail.com>
2020-09-14 08:27:08 +0300

    Initial commit'

---
+++ FUNCTION_POINTERS/PRIMER_1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int find_max(int *array, size_t size){
+    int max = *array;
+    for(volatile size_t i = 0; i < size; i++,array++){
+        if(*array > max) max = *array;
+    }
+
+    return max;
+}
+
+int find_n(int *array, size_t size, int(*find)(int*, size_t)){
+    int n = find(array,size);
+    return n;
+}
+
+int main(int argc, char* argv[]){
+
+    int marks[] = {1,34,54,2,43,67,54,78,96,23,15};
+
+    int maximum = find_n(marks,sizeof(marks)/sizeof(int),find_max);
+
+    std::cout<<"Maximum value: "<<maximum<<std::endl;
+
+
+    return 0;
+}
