df43ba3380286cdc06457dca26121f0112be280b
MCLEANS <jackagwaya@gmail.com>
2020-09-14 08:32:15 +0300

    added find_min implementation

--- FUNCTION_POINTERS/PRIMER_1/main.cpp
+++ FUNCTION_POINTERS/PRIMER_1/main.cpp
@@ -5,8 +5,15 @@ int find_max(int *array, size_t size){
     for(volatile size_t i = 0; i < size; i++,array++){
         if(*array > max) max = *array;
     }
-
     return max;
+}
+
+int find_min(int *array,size_t size){
+    int min = *array;
+    for(volatile size_t i = 0; i < size; i++,array++){
+        if(*array < min) min = *array;
+    }
+    return min;
 }
 
 int find_n(int *array, size_t size, int(*find)(int*, size_t)){
@@ -19,8 +26,11 @@ int main(int argc, char* argv[]){
     int marks[] = {1,34,54,2,43,67,54,78,96,23,15};
 
     int maximum = find_n(marks,sizeof(marks)/sizeof(int),find_max);
+    int minimum = find_n(marks,sizeof(marks)/sizeof(int),find_min);
+
 
     std::cout<<"Maximum value: "<<maximum<<std::endl;
+    std::cout<<"Minimum value: "<<minimum<<std::endl;
 
 
     return 0;
